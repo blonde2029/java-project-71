8d3fee7cfe952d6350ee6cb844214f4b95fbb146
blonde <mr_blonde@rambler.ru>
2023-09-20 17:23:51 +0700

    added options and parameters to command

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,19 +2,51 @@ package hexlet.code;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.concurrent.Callable;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
         description = "Compares two configuration files and shows a difference.")
-public class App implements Runnable {
-    //@Option(names = {"-h", "--help"}, description = "Show this help message and exit.")
+public class App implements Callable<Integer> {
+    @Option(names = {"-f", "--format"}, defaultValue = "", description = "output format [default: stylish]")
+    private String format;
+    @Parameters(index = "0", paramLabel = "filePath1", defaultValue = "", description = "path to first file")
+    private String filePath1;
+    @Parameters(index = "1", paramLabel = "filePath2", defaultValue = "", description = "path to second file")
+    private String filePath2;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         int exitCode = new CommandLine(new App()).execute(args);
         System.exit(exitCode);
     }
 
-    @Override
-    public void run() {
 
+    @Override
+    public Integer call() throws Exception {
+        String readFilePath = filePath1;
+        Path path = Paths.get(readFilePath).toAbsolutePath().normalize();
+
+        // Проверяем существование файла
+        if (!Files.exists(path)) {
+            try {
+                throw new Exception("File '" + path + "' does not exist");
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+        String content;
+        try {
+            content = Files.readString(path);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(content);
+        return null;
     }
 }
