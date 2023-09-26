30606d112a7411c3e9774cfdcd4c7823e364c941
blonde <mr_blonde@rambler.ru>
2023-09-26 11:05:19 +0700

    added YAML comparing

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,17 +1,16 @@
 package hexlet.code;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 
 public class Differ {
-    public static StringBuilder getDiff(String filePath1, String filePath2) throws IOException {
+    public static StringBuilder getDiffJSON(String filePath1, String filePath2) throws IOException {
         Path path1 = Paths.get(filePath1).toAbsolutePath().normalize();
         Path path2 = Paths.get(filePath2).toAbsolutePath().normalize();
         System.out.println(path1);
@@ -21,28 +20,13 @@ public class Differ {
         String content1 = Files.readString(path1);
         String content2 = Files.readString(path2);
 
-        Map<String, Object> data1 = getData(content1);
-        Map<String, Object> data2 = getData(content2);
+        Map<String, Object> data1 = Parser.getDataJSON(content1);
+        Map<String, Object> data2 = Parser.getDataJSON(content2);
         java.util.TreeMap<String, Object> allData = new java.util.TreeMap<>();
         allData.putAll(data1);
         allData.putAll(data2);
 
-        java.util.List<String> diffs = new java.util.ArrayList<>();
-        allData.entrySet().stream()
-                 .forEach(i -> {
-                     if (data1.containsKey(i.getKey()) && !data2.containsKey(i.getKey())) {
-                         diffs.add(" - " + i.getKey() + ": " + i.getValue());
-                     } else if (data1.containsKey(i.getKey()) && data2.containsKey(i.getKey())
-                             && data1.get(i.getKey()).equals(data2.get(i.getKey()))) {
-                         diffs.add("   " + i.getKey() + ": " + i.getValue());
-                     } else if (data1.containsKey(i.getKey()) && data2.containsKey(i.getKey())
-                             && !data1.get(i.getKey()).equals(data2.get(i.getKey()))) {
-                         diffs.add(" - " + i.getKey() + ": " + data1.get(i.getKey()));
-                         diffs.add(" + " + i.getKey() + ": " + i.getValue());
-                     } else if (!data1.containsKey(i.getKey()) && data2.containsKey(i.getKey())) {
-                         diffs.add(" + " + i.getKey() + ": " + i.getValue());
-                     }
-                 });
+        java.util.List<String> diffs = buildResult(data1, data2, allData);
          //выведем теперь в строку в нужном формате
         StringBuilder result = new StringBuilder();
         result.append("{\n");
@@ -50,17 +34,51 @@ public class Differ {
         result.append("}");
         return result;
     }
-    public static Map<String, Object> getData(String content) {
-        ObjectMapper mapper = new ObjectMapper();
-        Map<String, Object> data = null;
-        TypeReference<java.util.HashMap<String, Object>> typeReference = new TypeReference<>() { };
-        try {
-            data = mapper.readValue(content, typeReference);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        return data;
+    public static StringBuilder getDiffYAML(String filePath1, String filePath2) throws IOException {
+        Path path1 = Paths.get(filePath1).toAbsolutePath().normalize();
+        Path path2 = Paths.get(filePath2).toAbsolutePath().normalize();
+        System.out.println(path1);
+        isFileExists(path1);
+        isFileExists(path2);
+
+        String content1 = Files.readString(path1);
+        String content2 = Files.readString(path2);
+
+        Map<String, Object> data1 = Parser.getDataYAML(content1);
+        Map<String, Object> data2 = Parser.getDataYAML(content2);
+        java.util.TreeMap<String, Object> allData = new java.util.TreeMap<>();
+        allData.putAll(data1);
+        allData.putAll(data2);
+
+        java.util.List<String> diffs = buildResult(data1, data2, allData);
+        //выведем теперь в строку в нужном формате
+        StringBuilder result = new StringBuilder();
+        result.append("{\n");
+        diffs.stream().forEach(i -> result.append(i + "\n"));
+        result.append("}");
+        return result;
     }
+    private static List<String> buildResult(Map<String, Object> data1, Map<String, Object> data2,
+                                            TreeMap<String, Object> allData) {
+        java.util.List<String> diffs = new java.util.ArrayList<>();
+        allData.entrySet().stream()
+                .forEach(i -> {
+                    if (data1.containsKey(i.getKey()) && !data2.containsKey(i.getKey())) {
+                        diffs.add(" - " + i.getKey() + ": " + i.getValue());
+                    } else if (data1.containsKey(i.getKey()) && data2.containsKey(i.getKey())
+                            && data1.get(i.getKey()).equals(data2.get(i.getKey()))) {
+                        diffs.add("   " + i.getKey() + ": " + i.getValue());
+                    } else if (data1.containsKey(i.getKey()) && data2.containsKey(i.getKey())
+                            && !data1.get(i.getKey()).equals(data2.get(i.getKey()))) {
+                        diffs.add(" - " + i.getKey() + ": " + data1.get(i.getKey()));
+                        diffs.add(" + " + i.getKey() + ": " + i.getValue());
+                    } else if (!data1.containsKey(i.getKey()) && data2.containsKey(i.getKey())) {
+                        diffs.add(" + " + i.getKey() + ": " + i.getValue());
+                    }
+                });
+        return diffs;
+    }
+
     public static void isFileExists(Path path) {
         if (!Files.exists(path)) {
             try {
